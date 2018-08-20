ba527d98bca83b7bdd0121d002713a0c07427836
Siva <info.siva@gmail.com>
2018-08-20 22:13:34 +0100

    adding new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { HttpClientModule } from '@angular/common/http';
+import { QuizService } from './services/quiz.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [QuizService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+const configData = {
+    'ExerciseAPIURL' : 'http://localhost:4000/api/exercise'
+};
+export default configData;
